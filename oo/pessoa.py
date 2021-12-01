c86677ed753d4659b8e155e152e114bce54d11c5
Digi <wander.willazevedo@gmail.com>
2021-12-01 10:21:28 -0300

    Criado Metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar (self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print (Pessoa.cumprimentar(p))
-    print (id(p))
-    print (p.cumprimentar())
+    p = Pessoa('Wander')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'William'
+    print(p.nome)
